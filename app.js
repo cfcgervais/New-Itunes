3a3ed96d78c1886d4cc797640fc50a36776958c1
Tyler Gervais <cfcgervais@gmail.com>
2016-09-29 11:48:05 -0600

    updated

--- app.js
+++ app.js
@@ -3,30 +3,38 @@ function getMusic(){
   itunes.getMusicByArtist(artist).then(drawSongs);
 }
 
-
-
 function drawSongs(arr){ 
   var template = ''
   var songElem = document.getElementById('songList')
+  var index = 0;
   for (var i = 0; i < arr.length; i++) {
     var song = arr[i]
     template += `<div class="songs">
         <div class="song-container">
-          <div class="img-container">
-            <img src="${song.albumArt}"/>
-          </div>
-          <ul id="song-list">
-            <h3>${song.artist}</h3>
-            <h5>${song.title}</h5>
-            <audio controls>
-                <source src="${song.preview}">
-            </audio>
-          </ul>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="img-container">
+                        <img src="${song.albumArt}"/>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <ul id="song-list">
+                        <h2>${song.artist}</h2>
+                        <h4>${song.title}</h4>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <i class="fa fa-play fa-4x playButton" onclick="document.getElementById('${index}').play();$(this).toggle();$(this).next().toggle()" id="${index}play"></i>
+                    <i class="fa fa-pause fa-4x playButton" onclick="document.getElementById('${index}').pause();$(this).toggle();$('#${index}play').toggle()" style="display: none;"></i>
+                    <audio id="${index}" preload="none" >
+                        <source src="${song.preview}">
+                    </audio>
+                </div>
+            </div>
         </div> 
       </div>`
+      index++
   } 
   songElem.innerHTML = template 
-  
-//  drawSongs(songList)
-  // console.log(songList);
 }
+
